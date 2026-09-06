d652ebf5d054fe36b5f274664dbe43e2e0b44ef6
SURYA M K <suryapersonal2007@gmail.com>
2026-09-06 23:36:40 +0530

    Update API_URL to use environment variablea

--- login-app/src/App.jsx
+++ login-app/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = "https://login-i7wy.onrender.com/";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 function App() {
   // =====================================================
