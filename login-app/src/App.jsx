a0dba6b55b15e53c1a7be9d769900e88ce7e00dc
SURYA M K <suryapersonal2007@gmail.com>
2026-09-06 23:35:57 +0530

    api key changing

--- login-app/src/App.jsx
+++ login-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://login-i7wy.onrender.com/";
 
 function App() {
   // =====================================================
